769b4ab17d565ee3265c2c251ffd12001c6a91f1
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-07-05 10:18:17 +0100

    tests working. Tasks automatically set to complete: false

--- test/taskTest.js
+++ test/taskTest.js
@@ -1,17 +1,19 @@
 var assert = require('chai').assert;
 var expect = require('chai').expect;
 var Task = require('../lib/task.js');
-console.log(Task);
+
 
 describe('Task', function() {
-  // var task;
   // beforeEach(function() {
   //   var task = new Task('phone grandma');
   // });
   it('should store a task when constructed', function() {
     var task = new Task('phone grandma');
-    console.log(task._task);
-    console.log(task.get());
     expect(task.get()).to.equal('phone grandma');
+  });
+
+  it('task completion is set to false by default ', function() {
+    var task = new Task('phone grandma');
+    expect(task.status()).to.be.false;
   });
 });
