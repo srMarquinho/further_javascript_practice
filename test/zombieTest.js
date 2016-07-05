f4eaeb6bb6b2ca76c516a4bfda028890f5b76645
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-07-05 18:04:27 +0100

    zombie - oh no

--- test/zombieTest.js
+++ test/zombieTest.js
@@ -1,3 +1,5 @@
+process.env.NODE_ENV = 'test';
+
 'use strict';
 
 var app = require('http-server').createServer();
@@ -15,7 +17,12 @@ describe('Homepage', function() {
  });
 
  it('should show a title', function() {
-   this.browser.assert.text('title', 'To Do List')
+   this.browser.assert.text('title', 'To Do List');
+ });
+
+ it('should show some to dos', function() {
+  //  this.browser.fill('task', 'phone grandma').pressButton('Add Task');
+   this.browser.assert.text('li', 'phone grandma');
  });
 
 });
