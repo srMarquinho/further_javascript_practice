620ca72f5757c4da47cbaee6906b9e35e517aac0
Graham Ormiston <grahamormiston@gmail.com>
2016-07-06 16:14:57 +0100

    Added event listener to prevent default on click behaivour

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ var list = new List();
 list.addItem('phone grandma');
 list.addItem('eat chocolate');
 
+function stopDefAction(evt){
+	evt.preventDefault();
+}
 
 window.onload = function() {
 	document.getElementById('list-printed').innerHTML = printItems(list.list);
+	document.getElementById('submit').addEventListener('click', stopDefAction, false)
 }
-
-	
